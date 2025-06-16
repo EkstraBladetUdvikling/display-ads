e200ecbfb56ba52f0204f686da429775b7993cb1
tegner <jtegner@gmail.com>
2025-06-16 08:56:35 +0200

    safeguard

--- src/lib/util/removeplacement.ts
+++ src/lib/util/removeplacement.ts
@@ -1,4 +1,8 @@
 export function removePlacement(tagId: string) {
-	// console.log(`Removing ad unit with tagId: ${tagId}`, window.lwhb);
-	if (window.lwhb) window.lwhb.removeAdUnit(tagId);
+	try {
+		// console.log(`Removing ad unit with tagId: ${tagId}`, window.lwhb);
+		if (window.lwhb && window.lwhb.removeAdUnit) window.lwhb.removeAdUnit(tagId);
+	} catch (error) {
+		console.error(`Error removing ad unit with tagId: ${tagId}`, error);
+	}
 }
