fe3e5bdb955da527382ae149603bfe57d66550a9
tegner <jtegner@gmail.com>
2026-02-04 14:54:08 +0100

    remove unused code

--- src/lib/util/addplacement.ts
+++ src/lib/util/addplacement.ts
@@ -1,50 +1,7 @@
 import { logger } from '../logger';
 import { adsInterface } from '../init';
 import { BANNERSTATE } from '../state';
-import type { ICustomPlacement, IDefineTag, ILoadAdData } from '../types';
-
-export function addCustomPlacement(customplacement: ICustomPlacement, byPassLW: boolean = false) {
-	try {
-		const { adUnitName, allowedMediaTypes, callbackMethod, invCode, gamSizes, sizes, tagId } =
-			customplacement;
-
-		if (!byPassLW && adUnitName && tagId) {
-			window.lwhb.cmd.push(() => {
-				const loadAdData: ILoadAdData = {
-					adUnitName,
-					callbackMethod,
-					tagId
-				};
-
-				if (allowedMediaTypes) {
-					loadAdData.allowedMediaTypes = allowedMediaTypes;
-				}
-				if (sizes) loadAdData.sizes = sizes;
-				if (gamSizes) loadAdData.gamSizes = gamSizes;
-
-				if (BANNERSTATE.renderCalled) {
-					window.lwhb.loadAd(loadAdData);
-				} else {
-					window.lwhb.prepareAd(loadAdData);
-				}
-			});
-		} else if (byPassLW && invCode && gamSizes && tagId) {
-			window.googletag.cmd.push(() => {
-				window.googletag.defineSlot(invCode, gamSizes, tagId).addService(window.googletag.pubads());
-
-				window.googletag.enableServices();
-				window.googletag.pubads().refresh();
-			});
-		} else {
-			throw new Error('Wrong ad data');
-		}
-	} catch (error) {
-		console.error({
-			component: 'admanager.addCustomPlacement',
-			message: (error as Error).message
-		});
-	}
-}
+import type { IDefineTag, ILoadAdData } from '../types';
 
 interface IAddPlacementInput {
 	consent: boolean;
